6b8385bb8344785f6841ad23e865b7b7ef43e709
hevipiude <veerapir@hotmail.com>
2022-09-27 13:47:06 +0300

    puhelinluettelo2

--- T1_2/puhelinl.js
+++ T1_2/puhelinl.js
@@ -1,17 +1,3 @@
-/**Tee puhelinluettelo.
-Puhelinluetteloon lisäät taulukkoon objekteja (eli henkilöitä joilla nimi ja puhelinnumero).
-Käyttäjältä kysytään henkilön nimi ja puhelinnumero.
-Henkilön poisto -toimintoa ei tarvitse tässä versiossa olla.
-
-Tee hakutoiminto jossa haet nimen perusteella puhelinnumeron.
-
-Tee puhelinnumeron haku funktioksi.
-Funktion parametrina on taulukko josta haetaan ja henkilön nimi.
-Funktio palauttaa puhelinnumeron.
-Kutsu funktiota.
-
-Käyttöliittymän voit tehdä millaiseksi haluat (komentokehoite -pohjainen kuitenkin) */
-
 var input = require("readline-sync");
 var nimi = input.question("Anna nimi:");
 var numero = input.question("Anna puhelinnumero:");

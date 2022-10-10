19541bb7fab06757e471cbd9e5c4d5964f8280c5
hevipiude <veerapir@hotmail.com>
2022-10-10 15:36:59 +0300

    readfilesync

--- REST1/app.js
+++ REST1/app.js
@@ -1,19 +1,11 @@
 const fs = require("fs");
-const app = fs();
-const port = 3000;
-
-const fs = require("fs");
 
 try {
-  const data = fs.readFileSync("/Users/joe/test.txt", "utf8");
+  const data = fs.readFileSync(
+    "/Users/veera/Documents/Webi/REST1/sanakirja.txt",
+    "utf8"
+  );
   console.log(data);
 } catch (err) {
   console.error(err);
 }
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.listen(port, () => {
-  console.log(`example app listening on port ${port}`);
-});
